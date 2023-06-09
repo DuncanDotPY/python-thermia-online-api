b76e377af419ff0d2bfb76897837c9506d2006d0
Duncan Mathews <96064472+DuncanDotPY@users.noreply.github.com>
2023-06-09 15:34:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,6 @@ from pathlib import Path
 current_directory = Path(__file__).parent
 long_description = (current_directory / "README.md").read_text()
 
-version = sys.argv[3:]
-if version:
-    version = str(version[0])
-    sys.argv.remove(version)
-else:
-    raise Exception("Version is not set")
 
 setup(
     name="ThermiaOnlineAPI",
@@ -22,15 +16,14 @@ setup(
         "ThermiaOnlineAPI.model",
         "ThermiaOnlineAPI.utils",
     ],
-    version=version,
     license="GPL-3.0",
     description="A Python API for Thermia heat pumps using https://online.thermia.se",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Krisjanis Lejejs",
     author_email="krisjanis.lejejs@gmail.com",
-    url="https://github.com/klejejs/python-thermia-online-api",
-    download_url="https://github.com/klejejs/python-thermia-online-api/releases",
+    url="https://github.com/DuncanDotPY/python-thermia-online-api",
+    download_url="https://github.com/DuncanDotPY/python-thermia-online-api/releases",
     keywords=["Thermia", "Online"],
     install_requires=[],
     classifiers=[],
